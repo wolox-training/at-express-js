6f94a865be780c79570bf0320a8455899f965548
ana turrillo <ana.turrillo@wolox.com.ar>
2019-08-09 17:25:49 -0300

    removed file to unify services

--- app/services/get_albums.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const endpoint = 'https://jsonplaceholder.typicode.com/albums';
-
-exports.getAlbums = () => axios.get(endpoint);
-
-exports.getAlbum = albumId => axios.get(`${endpoint}/${albumId}`);
