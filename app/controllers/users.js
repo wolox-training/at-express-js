b734f76ac7f837da33c13f89936db902d1dda9f1
ana turrillo <ana.turrillo@wolox.com.ar>
2019-08-13 12:26:25 -0300

    adjustment in signup service response

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,7 +5,7 @@ exports.signUp = (req, res, next) =>
   hashPassword(req.body)
     .then(hashedUser => user.create(hashedUser))
     .then(result => {
-      const { id } = result.dataValues;
-      res.send({ message: `created new user with id: ${id}`, id });
+      const { firstName } = result.dataValues;
+      res.send({ message: `A user "${firstName}" has been created` });
     })
     .catch(next);
