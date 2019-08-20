95aecc43a38ef10cff77187e4a1064ef0f00736b
ana turrillo <ana.turrillo@wolox.com.ar>
2019-08-20 16:30:55 -0300

    user signup service test

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -1,14 +1,11 @@
 const { userSchema } = require('../schemas/userSchema');
 const { hashPassword } = require('./encryption');
 const { dbErrorCodes } = require('./dbErrorCodes');
-const { EMAIL_REGEX, SALT_ROUNDS, EMAIL_DOMAIN, PASSWORD_FORMATS, MIN_LENGTH } = require('./constants');
+const CONSTANTS = require('./constants');
+
 module.exports = {
   hashPassword,
   dbErrorCodes,
   userSchema,
-  EMAIL_REGEX,
-  SALT_ROUNDS,
-  EMAIL_DOMAIN,
-  PASSWORD_FORMATS,
-  MIN_LENGTH
+  ...CONSTANTS
 };
--- test/user.spec.js
+++ test/user.spec.js
@@ -1,11 +1,16 @@
 const chance = require('chance')();
 const supertest = require('supertest');
 const { expect } = require('chai');
-// eslint-disable-next-line no-unused-vars
-const should = require('chai').should();
 const bcrypt = require('bcryptjs');
 const app = require('../app');
 const { User } = require('../app/models');
+const {
+  alreadyExistsErrorMessage,
+  invalidPasswordLengthMessage,
+  invalidPasswordMessage,
+  invalidEmailDomain,
+  missingRequiredFieldsMessage
+} = require('../app/helpers');
 
 const request = supertest(app);
 const mockUser = {
@@ -16,12 +21,12 @@ const mockUser = {
 };
 
 describe('POST /users', () => {
-  it('It should respond with code 201 when data passes validation checks', () =>
+  it('should create a new user and respond with code 201 when data passes validation checks', () =>
     request
       .post('/users')
       .send(mockUser)
       .then(res => {
-        expect(res.status).to.equal(201);
+        expect(res.statusCode).to.equal(201);
         return User.findOne({ where: { email: mockUser.email } });
       })
       .then(user => {
@@ -29,10 +34,137 @@ describe('POST /users', () => {
         return Promise.all([user, arePasswordsEqual]);
       })
       .then(([user, arePasswordsEqual]) => {
-        // eslint-disable-next-line no-unused-expressions
-        expect(arePasswordsEqual).to.be.true;
+        expect(arePasswordsEqual).to.equal(true);
         expect(user.firstName).to.equal(mockUser.firstName);
         expect(user.lastName).to.equal(mockUser.lastName);
         expect(user.email).to.equal(mockUser.email);
       }));
+
+  it('should respond with 422 when email already exists', () =>
+    request
+      .post('/users')
+      .send(mockUser)
+      .then(() => request.post('/users').send(mockUser))
+      .then(response => {
+        expect(response.body.message).to.include(alreadyExistsErrorMessage);
+        expect(response.statusCode).to.equal(422);
+      }));
+
+  it('should respond with 422 when password is badly formatted (too short)', () => {
+    const errorUser = {
+      ...mockUser,
+      password: chance.word({ length: 2 }) + chance.integer({ min: 0, max: 9 })
+    };
+    request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        expect(response.body.message).to.include(invalidPasswordLengthMessage);
+        expect(response.statusCode).to.equal(422);
+      });
+  });
+
+  it('should respond with 422 when password is badly formatted (not containing letters and numbers)', () => {
+    const password = chance.word({ length: 10 });
+    const errorUser = {
+      ...mockUser,
+      password
+    };
+    return request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        expect(response.body.message).to.include(invalidPasswordMessage);
+        expect(response.statusCode).to.equal(422);
+      });
+  });
+
+  it('should respond with 422 when email is not @wolox.com.ar', () => {
+    const errorUser = {
+      ...mockUser,
+      email: chance.email({ domain: 'different.com.ar' })
+    };
+    return request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        expect(response.body.message).to.include(invalidEmailDomain);
+        expect(response.statusCode).to.equal(422);
+      });
+  });
+
+  it('should respond with 400 when firstName is missing', () => {
+    const errorUser = {
+      ...mockUser,
+      firstName: null
+    };
+    return request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        expect(response.body.message).to.include(missingRequiredFieldsMessage);
+        expect(response.body.message).to.include('firstName');
+        expect(response.statusCode).to.equal(400);
+      });
+  });
+
+  it('should respond with 400 when lastName is missing', () => {
+    const errorUser = {
+      ...mockUser,
+      lastName: null
+    };
+    return request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        expect(response.body.message).to.include(missingRequiredFieldsMessage);
+        expect(response.body.message).to.include('lastName');
+        expect(response.statusCode).to.equal(400);
+      });
+  });
+
+  it('should respond with 400 when email is missing', () => {
+    const errorUser = {
+      ...mockUser,
+      email: null
+    };
+    return request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        expect(response.body.message).to.include(missingRequiredFieldsMessage);
+        expect(response.body.message).to.include('email');
+        expect(response.statusCode).to.equal(400);
+      });
+  });
+
+  it('should respond with 400 when password is missing', () => {
+    const errorUser = {
+      ...mockUser,
+      password: null
+    };
+    return request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        expect(response.body.message).to.include(missingRequiredFieldsMessage);
+        expect(response.body.message).to.include('password');
+        expect(response.statusCode).to.equal(400);
+      });
+  });
+
+  it('should list all missing fields', () => {
+    const errorUser = {};
+    return request
+      .post('/users')
+      .send(errorUser)
+      .then(response => {
+        const msg = response.body.message;
+        expect(msg).to.include(missingRequiredFieldsMessage);
+        expect(msg).to.include('firstName');
+        expect(msg).to.include('lastName');
+        expect(msg).to.include('email');
+        expect(msg).to.include('password');
+      });
+  });
 });
