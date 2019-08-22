0f656fc0a0d3541e028723ead46499d835dd2c31
ana turrillo <ana.turrillo@wolox.com.ar>
2019-08-22 18:31:15 -0300

    minor fix

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -4,7 +4,7 @@ const app = require('../../app');
 const request = supertest(app);
 
 exports.createUsers = count => {
-  const createArray = (arr, counting) => {
+  const createRequestArray = (arr, counting) => {
     const mockUser = {
       firstName: chance.first(),
       lastName: chance.last(),
@@ -15,9 +15,9 @@ exports.createUsers = count => {
     if (counting === 1) {
       return [...arr, request.post('/users').send(mockUser)];
     }
-    return createArray([...arr, request.post('/users').send(mockUser)], counting - 1);
+    return createRequestArray([...arr, request.post('/users').send(mockUser)], counting - 1);
   };
 
-  const promises = createArray([], count);
+  const promises = createRequestArray([], count);
   return Promise.all(promises);
 };
