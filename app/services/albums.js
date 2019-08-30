fc1aa6eb7c029c108c40a17d880db4411d59ba27
ana turrillo <ana.turrillo@wolox.com.ar>
2019-08-30 11:19:12 -0300

    fixes by code review

--- app/services/albums.js
+++ app/services/albums.js
@@ -4,4 +4,4 @@ const { handleError } = require('../services/commons/errorHandler');
 exports.createUserAlbum = userId => album =>
   UserAlbum.createAlbum(album.id, userId).catch(handleError('Unable to create new album'));
 
-exports.getUserAlbums = userId => UserAlbum.findBy({ userId });
+exports.getUserAlbums = userId => UserAlbum.findBy({ userId }).catch(handleError('Unable to find album'));
